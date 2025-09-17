858d902c1e5b7f6e80afe3766ad55929e659076c
de180334 <khoa2k4hg@gmail.com>
2025-09-17 07:26:42 +0700

    view,search , filter, saved tutor

--- learnmatefe/src/Layout.js
+++ learnmatefe/src/Layout.js
@@ -1,13 +1,29 @@
 import React, { Suspense } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
-
-
-
+import TutorListPage from "./pages/Booking/tutorPage";
+import SavedTutorsPage from "./pages/Booking/SavedTutorPage";
+import Header from "./pages/Layout/Header/Header";
+import Footer from "./pages/Layout/Footer/Footer";
+import ProtectedRoute from "./ProtectRoutes";
 const AppLayout = ({ children }) => {
+  const location = useLocation();
+  const noHeaderFooterRoutes = ["/otp-verify", "/auth/callback"];
+  const hideHeaderFooter = noHeaderFooterRoutes.includes(location.pathname);
+  useEffect(() => {
 
+  }, [])
+  return (
+    <>
+      {!hideHeaderFooter && <Header />}
+      {children}
+      {!hideHeaderFooter && <Footer />}
+    </>
+  );
 };
 const Layout = () => {
   useEffect(() => {
@@ -30,7 +46,26 @@ const Layout = () => {
       />
       <BrowserRouter>
        <Routes>
-        
+       <Route
+            path="/tutor"
+            element={
+              <ProtectedRoute allowedRoles={['student', 'admin','tutor']}>
+                <AppLayout>
+                  <TutorListPage />
+                </AppLayout>
+              </ProtectedRoute>}
+          />
+          <Route
+            path="/saved-tutors"
+            element={
+              <ProtectedRoute allowedRoles={['student','tutor']}>
+                <AppLayout>
+                  <SavedTutorsPage />
+                </AppLayout>
+
+              </ProtectedRoute>
+            }
+          />
        </Routes>
       </BrowserRouter>
     </Suspense>
