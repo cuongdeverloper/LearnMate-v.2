79448e912c043d18d0aae3c92b1fe7d1721bf6c6
de180334 <khoa2k4hg@gmail.com>
2025-09-29 21:53:50 +0700

    update modal answer, assignment / submission , material, question , quiz

--- LearnMateBe/src/modal/Quiz.js
+++ LearnMateBe/src/modal/Quiz.js
@@ -2,6 +2,16 @@
 const mongoose = require("mongoose");
 
 const QuizSchema = new mongoose.Schema({
+  subjectId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subject',
+    required: true
+  },
+  tutorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tutor',
+    required: true
+  },
   title: { type: String, required: true },
   subject: { type: mongoose.Schema.Types.ObjectId, ref: "Subject" },
   tutor: { type: mongoose.Schema.Types.ObjectId, ref: "Tutor" },
