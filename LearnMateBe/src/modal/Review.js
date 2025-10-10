dd3f0349974bf59e7f31dcf41ef8d78cd2e471f2
de180334 <khoa2k4hg@gmail.com>
2025-10-10 08:49:22 +0700

    edit modal

--- LearnMateBe/src/modal/Review.js
+++ LearnMateBe/src/modal/Review.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ReviewSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   tutor: { type: mongoose.Schema.Types.ObjectId, ref: 'Tutor' }, // Có thể null nếu review cho course
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Material' }, // Có thể null nếu review cho tutor
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }, // Có thể null nếu review cho tutor
   rating: { type: Number, min: 1, max: 5, required: true },
   comment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
