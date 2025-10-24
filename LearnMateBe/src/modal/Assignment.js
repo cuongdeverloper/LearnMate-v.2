5ab0f3448fe70e8b9f996e08a3a65d32b1ae1c25
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2025-10-24 21:40:48 +0700

    fix modal

--- LearnMateBe/src/modal/Assignment.js
+++ LearnMateBe/src/modal/Assignment.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const assignmentSchema = new mongoose.Schema({
-  subjectId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Subject',
-    required: true
-  },
-  tutorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tutor', required: true },
-  learnerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  bookingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
-  title: { type: String, required: true },
+const AssignmentSchema = new mongoose.Schema({
+  tutorId: { type: mongoose.Schema.Types.ObjectId, ref: "Tutor", required: true },
+  learnerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  bookingId: { type: mongoose.Schema.Types.ObjectId, ref: "Booking", required: true },
+  subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject", required: true },
+  templateId: { type: mongoose.Schema.Types.ObjectId, ref: "AssignmentTemplate" },
+
+  title: String,
   description: String,
   fileUrl: String,
   deadline: Date,
 }, { timestamps: true });
 
-module.exports = mongoose.model('Assignment', assignmentSchema);
+module.exports = mongoose.model("Assignment", AssignmentSchema);
